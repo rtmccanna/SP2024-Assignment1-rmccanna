9b0ebcca6357d985dfcd6d6e99992c520eed507e
robmc <robmc@RTM_LAPTOP24>
2024-09-10 16:47:45 -0500

    Add project files.

---
+++ SP2024-Assignment1-rmccanna/Program.cs
@@ -0,0 +1,16 @@
+namespace SP2024_Assignment1_rmccanna
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
